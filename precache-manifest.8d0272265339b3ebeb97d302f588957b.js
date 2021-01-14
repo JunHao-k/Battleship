bbfaec80d41f674da96a753e5eb650aa39986985
Jun Hao <jun_hao_koh@hotmail.com>
2021-01-14 19:45:11 +0800

    Updates

---
+++ precache-manifest.8d0272265339b3ebeb97d302f588957b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a3ac17bb31dc6038ea794c700aad683d",
+    "url": "/Battleship/index.html"
+  },
+  {
+    "revision": "6181599619a5059a32be",
+    "url": "/Battleship/static/css/main.06d6534d.chunk.css"
+  },
+  {
+    "revision": "af2ad425f4428b2a5519",
+    "url": "/Battleship/static/js/2.2e30c30e.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/Battleship/static/js/2.2e30c30e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6181599619a5059a32be",
+    "url": "/Battleship/static/js/main.a3935399.chunk.js"
+  },
+  {
+    "revision": "bdbc7ae9adcd001a97c2",
+    "url": "/Battleship/static/js/runtime-main.4116cd34.js"
+  },
+  {
+    "revision": "7df61d3e56c702de5bc1fed80f5cc438",
+    "url": "/Battleship/static/media/BG.7df61d3e.jpg"
+  }
+]);
